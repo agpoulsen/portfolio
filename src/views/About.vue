04679cbfa591b6ac43e532be4b28ed156767962b
Alex Poulsen <lexpoulsen@gmail.com>
2021-11-28 13:51:58 +1100

    Created application

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>I am a software engineering student from General Assembly with over a decade in the brewing and distilling industry, where I was responsible for the creation of award-winning products.
+Coming from this industry, I am no stranger to problem-solving, effective communication and high standards of work.
+I have always held a keen interest in technology and now seek to engage with larger, more complex issues.
+Now looking for a challenge where I can combine my newfound technical skills and contribute to a team that values collaboration, culture and harmony.</p>
   </div>
 </template>
+
+<script>
+
+</script>
+
+<style >
+  p {
+    justify-content: center;
+  }
+</style>
